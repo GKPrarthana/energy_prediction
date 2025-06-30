a25244ed22527b63bdf802a30fd7b445f23b7143
Prarthana Ganegama <105261574+GKPrarthana@users.noreply.github.com>
2025-07-01 02:21:10 +0530

    setup.py added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='appliance_energy_predictor',
+    version='0.0.0',
+    author='Prarthana', 
+    author_email='39-dba-0025@kdu.ac.lk', 
+    packages=find_packages(),
+    install_requires=[]
+)
