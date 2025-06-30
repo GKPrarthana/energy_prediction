ddff8dc35301db56b185c9dae51606a3f1402106
Prarthana Ganegama <105261574+GKPrarthana@users.noreply.github.com>
2025-07-01 02:25:03 +0530

    main updated

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.appliance_energy_predictor.pipeline.training_pipeline import run_pipeline
+from appliance_energy_predictor.pipeline.training_pipeline import run_pipeline
 
 if __name__ == "__main__":
     print("Starting the Appliance Energy Prediction pipeline...")
