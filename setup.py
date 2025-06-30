79eccd73e3eea0623c8aeaaa00386533a30b59c8
Prarthana Ganegama <105261574+GKPrarthana@users.noreply.github.com>
2025-07-01 02:23:59 +0530

    setup updated

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import find_packages, setup
 setup(
     name='appliance_energy_predictor',
     version='0.0.0',
-    author='Prarthana', 
-    author_email='39-dba-0025@kdu.ac.lk', 
-    packages=find_packages(),
+    author='Prarthana Sewmini Ganegama',
+    author_email='39-dba-0025@kdu.ac.lk',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
     install_requires=[]
 )
